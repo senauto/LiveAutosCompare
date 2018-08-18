237766dec494894bc1186d50ad87a48e393a538a
senauto <james.seb7@gmail.com>
2018-08-18 12:55:31 +0100

    Initial Commit of Base project

---
+++ src/main/java/com/autodealers/CarSearch.java
@@ -0,0 +1,10 @@
+package main.java.com.autodealers;
+
+public class CarSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Car search and Comparison Engine ");
+	}
+
+}
